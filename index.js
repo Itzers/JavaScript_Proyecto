67bda857bd690306e835125b93d694ee5aa2b83c
Itzers <inyach@gmail.com>
2022-03-29 22:41:48 -0600

    Segunda entrega del proyecto Final

--- index.js
+++ index.js
@@ -1,4 +1,74 @@
-alert (`Bienvenidx!`);
+const opciones = document.getElementById('opciones')
+const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
+let carrito = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+   fetchData()
+})
+
+opciones.addEventListener('click', e => {
+   addCarrito(e)
+})
+
+const fetchData = async () => {
+   try {
+      const res = await fetch ('index.json')
+      const data = await res.json()
+      //console.log(data)
+      pintarCards(data)
+   } catch (error) {
+      console.log(error)
+   }
+}
+
+const pintarCards = data => {
+   data.forEach(producto => {
+      templateCard.querySelector('h5').textContent = producto.title 
+      templateCard.querySelector('p').textContent = producto.precio
+      templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
+      templateCard.querySelector('.btn-dark').dataset.id = producto.id
+      const clone = templateCard.cloneNode(true)
+      fragment.appendChild(clone)
+   })
+   opciones.appendChild(fragment)
+}
+
+const addCarrito = e => {
+   //console.log(e.target)
+   //console.log(e.target.classList.contains('btn-dark'))
+   if (e.target.classList.contains('btn-dark')) {
+      setCarrito(e.target.parentElement)
+   }
+   e.stopPropagation()
+}
+
+const setCarrito = objeto => {
+   //console.log(objeto)
+   const producto = {
+      id: objeto.querySelector('.btn-dark').dataset.id,
+      title: objeto.querySelector('h5').textContent,
+      precio: objeto.querySelector('p').textContent,
+      cantidad: 1
+   }
+
+   if(carrito.hasOwnProperty(producto.id)) {
+      producto.cantidad = carrito[producto.id].cantidad + 1
+   }
+
+   carrito[producto.id] = {...producto}
+
+   console.log(carrito)
+}
+
+
+
+
+
+
+
+
+/*alert (`Bienvenidx!`);
 
 function solicitarNombre () {
 let nombre = prompt ("Cual es tu nombre");
@@ -17,7 +87,7 @@ function Masaje (masajista,estilo,horario){
 }
 const masaje1 = new Masaje ("Juan","quiropractico",14);
 const masaje2 = new Masaje ("María","relajante",12);
-const masaje3 = new Masaje ("Julieta","quiropraxia",12);
+const masaje3 = new Masaje ("Julieta","quiropraxia",12);*/
 
 
 
